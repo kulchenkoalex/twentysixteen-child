7bccfa0268680c2aa98a009132f8e2577e20fa28
kulchenkoalex <kulchenko.alexey@gmail.com>
2017-03-15 16:46:06 +0200

    Локализация дочерних тем

--- functions.php
+++ functions.php
@@ -16,4 +16,27 @@ function my_theme_styles() {
 	wp_enqueue_style('child-theme-css', get_stylesheet_directory_uri() .'/style.css', array('parent-theme-css') );
 }
 
+define("MY_THEME_TEXTDOMAIN", 'twentysixteen-child');
+
+/**
+ * Загрузка Text Domain
+ */
+function childThemeLocalization(){
+    load_child_theme_textdomain(MY_THEME_TEXTDOMAIN, get_stylesheet_directory() . '/languages/');
+}
+add_action('after_setup_theme', 'childThemeLocalization');
+
+add_action('admin_menu', 'addAdminMenu');
+function addAdminMenu(){
+    $themeMenuPage = add_theme_page(
+        __('Sub theme Twentysixteen child', MY_THEME_TEXTDOMAIN),
+        __('Sub theme Twentysixteen child', MY_THEME_TEXTDOMAIN),
+        'read',
+        'twentysixteen_child_control_sub_theme_menu',
+        'renderThemeMenu'
+    );
+}
+function renderThemeMenu(){
+    _e('Sub theme Twentysixteen child', MY_THEME_TEXTDOMAIN);
+}
 ?>

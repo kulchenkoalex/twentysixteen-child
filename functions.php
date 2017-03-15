da932374c71d71f257aabe1d8abef5c3e833b8e3
kulchenkoalex <kulchenko.alexey@gmail.com>
2017-03-15 15:50:37 +0200

    Подключение style.css родительской темы

--- functions.php
+++ functions.php
@@ -10,4 +10,10 @@ function filterTheContent($content) {
     return $content.' - Twenty Sixteen Child';
 }
 
+add_action('wp_enqueue_scripts', 'my_theme_styles' );
+function my_theme_styles() {
+	wp_enqueue_style('parent-theme-css', get_template_directory_uri() .'/style.css' );
+	wp_enqueue_style('child-theme-css', get_stylesheet_directory_uri() .'/style.css', array('parent-theme-css') );
+}
+
 ?>
